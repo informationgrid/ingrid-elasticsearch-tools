7631729ca87498a15f940f558e61fe19697bb5d6
Jan Buecher <51136390+John-Books@users.noreply.github.com>
2022-02-16 17:08:51 +0100

    Use default field for Phrase-Search

--- src/main/java/de/ingrid/elasticsearch/search/converter/QueryConverter.java
+++ src/main/java/de/ingrid/elasticsearch/search/converter/QueryConverter.java
@@ -7,12 +7,12 @@
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
  * EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl5
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -62,7 +62,7 @@ public class QueryConverter implements IQueryParsers {
 
     public QueryConverter() {
         _queryConverter = new ArrayList<>();
-        fieldBoosts = getFieldBoostMap(new String[] {"title.phrase", "summary.phrase","content.phrase"});
+        fieldBoosts = getFieldBoostMap(new String[] {"title^10", "summary^2","content"});
     }
 
     public void setQueryParsers(List<IQueryParsers> parsers) {
